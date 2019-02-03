eb8008631ba19c74c64ed54a4c9ee26cc9dce2b4
Zeeshan Zubair <zeeshann.zss@gmail.com>
2019-02-03 16:32:00 +0400

    Testing out ASP.NET Core request processing pipeline

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace KUDVENKAT.Practice.ASP.Net.Core.App
 {
@@ -25,17 +26,33 @@ namespace KUDVENKAT.Practice.ASP.Net.Core.App
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+                                ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW1: Incoming Request");
+                await next();
+                logger.LogInformation("MW1: Outgoing Response");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW2: Incoming Request");
+                await next();
+                logger.LogInformation("MW2: Outgoing Response");
+            });
+
             app.Run(async (context) =>
             {
                 await context.Response
-                .WriteAsync(_config["MyKey"]);
+                .WriteAsync("MW3: Request handled and response produced.");
+                logger.LogInformation("MW3: Request handled and response produced.");
             });
         }
     }
